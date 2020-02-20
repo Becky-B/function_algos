64300537e7a3b84a31260beabee472041d7a5092
Becky_B <bettsrebecca@yahoo.com>
2020-02-20 14:51:07 -0800

    functions3

--- 3_shift_left.js
+++ 3_shift_left.js
@@ -1,8 +1,18 @@
 // Given an array, shift all values one position to the left.  Change the final position to 0. Return the changed array
 // Predicted Output: shiftLeft([1,2,3,4,5]) should return [2,3,4,5,0];
 
-function shitftLeft(arr){
-    // YOUR CODE HERE
+function shiftLeft(arr){
+    var leftshift = 1;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i + leftshift] == undefined){
+            arr[i]= 0;
+        }
+        else{
+            arr[i] = arr[i + leftshift];
+        }
+        
+    }
+    return arr;
 }
 var resultArr = shiftLeft([1,2,3,4,5]);
 console.log(resultArr);
